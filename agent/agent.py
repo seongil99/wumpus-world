270b3a3005fc69839e276a128e12a66e21dd9a02
Seongil Yoon <books13456@gmail.com>
2023-05-14 16:47:59 +0900

    add return type in agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -9,21 +9,21 @@ class Agent:
         self.path = [(1, 1)]
         self.grid_state = []
 
-    def process_percepts(self, percepts):
+    def process_percepts(self, percepts) -> None:
         """
         에이전트는 이 메소드를 통해 주어진 센서 입력을 처리합니다.
         """
         # 센서 입력 처리 로직 구현
         pass
 
-    def decide_action(self):
+    def decide_action(self) -> None:
         """
         에이전트는 이 메소드를 통해 다음 행동을 결정합니다.
         """
         # 행동 결정 로직 구현
         pass
 
-    def restart(self):
+    def restart(self) -> None:
         self.arrow = 2
         self.x = 1
         self.y = 1
@@ -32,10 +32,10 @@ class Agent:
         self.dead = False
         self.path = []
 
-    def shoot(self):
+    def shoot(self) -> None:
         self.arrow -= 1
 
-    def move(self):
+    def move(self) -> None:
         if self.direction == "east":
             self.x += 1
         elif self.direction == "west":
@@ -47,7 +47,7 @@ class Agent:
 
         self.path.append((self.x, self.y))
 
-    def turn_left(self):
+    def turn_left(self) -> None:
         if self.direction == "east":
             self.direction = "north"
         elif self.direction == "west":
@@ -57,7 +57,7 @@ class Agent:
         elif self.direction == "south":
             self.direction = "east"
 
-    def turn_right(self):
+    def turn_right(self) -> None:
         if self.direction == "east":
             self.direction = "south"
         elif self.direction == "west":
@@ -67,8 +67,8 @@ class Agent:
         elif self.direction == "south":
             self.direction = "west"
 
-    def climb(self):
+    def climb(self) -> None:
         pass
 
-    def grab(self):
+    def grab(self) -> None:
         pass

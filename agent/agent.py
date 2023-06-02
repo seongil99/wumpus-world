12a946dc0f483bcbfb64686f111f7f4b69d05bd2
Seongil Yoon <books13456@gmail.com>
2023-06-02 13:26:36 +0900

    refector agnet constants and add agent sensor input

--- agent/agent.py
+++ agent/agent.py
@@ -1,16 +1,12 @@
+from const import *
 from grid.cell import Cell
 
 
 class Agent:
     def __init__(self) -> None:
-        self.EAST = 0
-        self.NORTH = 1
-        self.WEST = 2
-        self.SOUTH = 3
-        self.arrow = 2
         self.x = 1
         self.y = 1
-        self.direction = self.EAST
+        self.direction = EAST
         self.gold = False
         self.dead = False
         self.grid_state = [[Cell() for _ in range(6)] for _ in range(6)]
@@ -49,36 +45,36 @@ class Agent:
         self.arrow -= 1
 
     def move(self) -> None:
-        if self.direction == self.EAST:
+        if self.direction == EAST:
             self.x += 1
-        elif self.direction == self.WEST:
+        elif self.direction == WEST:
             self.x -= 1
-        elif self.direction == self.NORTH:
+        elif self.direction == NORTH:
             self.y += 1
-        elif self.direction == self.SOUTH:
+        elif self.direction == SOUTH:
             self.y -= 1
 
         self.path.append([self.x, self.y])
 
     def turn_left(self) -> None:
-        if self.direction == self.EAST:
-            self.direction = self.NORTH
-        elif self.direction == self.WEST:
-            self.direction = self.SOUTH
-        elif self.direction == self.NORTH:
-            self.direction = self.WEST
-        elif self.direction == self.SOUTH:
-            self.direction = self.EAST
+        if self.direction == EAST:
+            self.direction = NORTH
+        elif self.direction == WEST:
+            self.direction = SOUTH
+        elif self.direction == NORTH:
+            self.direction = WEST
+        elif self.direction == SOUTH:
+            self.direction = EAST
 
     def turn_right(self) -> None:
-        if self.direction == self.EAST:
-            self.direction = self.SOUTH
-        elif self.direction == self.WEST:
-            self.direction = self.NORTH
-        elif self.direction == self.NORTH:
-            self.direction = self.EAST
-        elif self.direction == self.SOUTH:
-            self.direction = self.WEST
+        if self.direction == EAST:
+            self.direction = SOUTH
+        elif self.direction == WEST:
+            self.direction = NORTH
+        elif self.direction == NORTH:
+            self.direction = EAST
+        elif self.direction == SOUTH:
+            self.direction = WEST
 
     def climb(self) -> None:
         pass
@@ -93,3 +89,15 @@ class Agent:
         path = []
         
         return path
+    
+    def get_sensor_input(self, sensor_input: list) -> None:
+        if sensor_input[0]:
+            self.grid_state[self.x][self.y].stench = True
+        if sensor_input[1]:
+            self.grid_state[self.x][self.y].breeze = True
+        if sensor_input[2]:
+            self.grid_state[self.x][self.y].glitter = True
+        if sensor_input[3]:
+            self.grid_state[self.x][self.y].bump = True
+        if sensor_input[4]:
+            self.grid_state[self.x][self.y].scream = True
--- const/__init__.py
+++ const/__init__.py
@@ -2,8 +2,17 @@ __all__ = [
     'SAFE',
     'DANGER',
     'UNKNOWN',
+    'EAST',
+    'NORTH',
+    'WEST',
+    'SOUTH'
 ]
 
 SAFE = 0
 DANGER = 1
 UNKNOWN = 2
+
+EAST = 0
+NORTH = 1
+WEST = 2
+SOUTH = 3
